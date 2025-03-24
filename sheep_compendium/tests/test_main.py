6db42bf1769c2990eee9c4df7080097e48aaeb25
Shyam <shyamie123@gmail.com>
2025-03-23 20:12:34 -0400

    update
    update

---
+++ sheep_compendium/tests/test_main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_read_sheep():
+    response = client.get("/sheep/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "Spice",
+        "breed": "Gotland",
+        "sex": "ewe"
+    }
